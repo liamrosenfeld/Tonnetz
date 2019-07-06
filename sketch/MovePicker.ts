c7e815597b462736ff101a9aabf3b5bc39bf2485
Liam Rosenfeld <liamrosenfeld@users.noreply.github.com>
2019-07-05 23:54:21 -0400

    Better Looking Buttons

--- sketch/MovePicker.ts
+++ sketch/MovePicker.ts
@@ -12,23 +12,32 @@ class MovePicker {
     this.sketch.textSize(20);
 
     this.sketch.text("Primary Operations", 1000, 100);
-    this.createButton("A - Leading Tone", 110, this.leadingTone);
-    this.createButton("S - Parallel", 150, this.parallel);
-    this.createButton("D - Relative", 190, this.relative);
+    this.createButton("Leading Tone", 110, this.leadingTone);
+    this.createButton("Parallel", 150, this.parallel);
+    this.createButton("Relative", 190, this.relative);
 
     this.sketch.text("Compound Operations", 1000, 280)
-    this.createButton("Z - Nebenverwandt Left (LLP)", 290, this.nebenLeft);
-    this.createButton("X - Nebenverwandt Right (RRP)", 330, this.nebenRight);
-    this.createButton("C - Slide (LPR)", 370, this.slide);
-    this.createButton("V - Haxatonic Pole (LPL)", 410, this.hexatonicPole);
+    this.createButton("Nebenverwandt Left (LLP)", 290, this.nebenLeft);
+    this.createButton("Nebenverwandt Right (RRP)", 330, this.nebenRight);
+    this.createButton("Slide (LPR)", 370, this.slide);
+    this.createButton("Haxatonic Pole (LPL)", 410, this.hexatonicPole);
+
+    this.sketch.textSize(15);
+    this.sketch.text("A", 1010, 129);
+    this.sketch.text("S", 1010, 169);
+    this.sketch.text("D", 1010, 209);
+    this.sketch.text("Z", 1010, 309);
+    this.sketch.text("X", 1010, 349);
+    this.sketch.text("C", 1010, 389);
+    this.sketch.text("V", 1010, 429);
   }
 
   createButton(text: string, y: number, callback: () => boolean) {
     let button = this.sketch.createButton(text);
-    button.position(1000, y);
+    button.position(1030, y);
     button.style('font-size', '12px');
     button.style('padding', '5px');
-    button.style('width', '200px');
+    button.style('width', '190px');
     button.style('text-align', 'left');
     button.mousePressed(callback);
   }
